16921b69282e681c8789863c3e37b0376f12a79d
ELMO Borges <elmo.borges@soterotech.com.br>
2024-07-25 18:25:50 -0300

    first commit

---
+++ frontend/src/components/Card.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+interface Card {
+  id: number;
+  name: string;
+  email: string;
+}
+
+const Card: React.FC<{ card: Card }> = ({ card }) => {
+    return (
+        <div className="bg-white shadow-lg rounded-lg p-2 mb-2 hover:bg-gray-100">
+            <div className="text-sm text-gray-600">Id: {card.id}</div>
+            <div className="text-lg font-semibold text-gray-800">{card.name}</div>
+            <div className="text-md text-gray-700">{card.email}</div>
+            <p>{card.email}</p>
+        </div>
+    )
+}
+
+export default Card
